76470fad6c2d5d5eabb0d0dc19e44154f3e950ca
Diego Jimenez <diegojmoravia@gmail.com>
2023-08-09 21:31:55 -0600

    Avance

--- index.php
+++ index.php
@@ -4,42 +4,67 @@ include 'includes/connect.php';
 //consulta
 $querydb = "SELECT * FROM cuestionario";
 $save = $connect->query($querydb);
+
+$sql = "SELECT * FROM `Contenidos` WHERE `numero` = 1";
+
+$result = $connect->query($sql);
+
+$numero = "";
+$descripcion = "";
+
+if ($result->num_rows > 0) {
+    // Obtener datos del registro
+    $row = $result->fetch_assoc();
+    $numero = $row["numero"];
+    $descripcion = $row["descripcion"];
+}
+
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
-		<meta charset="utf-8">
-        <title> Inicio </title>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-        <script src="js/jquery-3.7.0.min.js"></script>
+    <meta charset="utf-8">
+    <title> Inicio </title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.7.0.min.js"></script>
 </head>
 <body>
-	<script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
-	<h3 class="text-center"> Cuestionario de Objetivos de Control </h3>
-	<div class="table-responsive table/hover" id="TablaConsulta">
-		<table class="table">
-			<thead class="text-muted">
-				<th class="text-center">ID</th>
-				<th class="text-center">Objetivo</th>
-				<th class="text-center">Sí</th>
-				<th class="text-center">No</th>
-				<th class="text-center">N/A</th>
-			</thead>
-			<tbody>
-				<?php while($row = $save->fetch_assoc()){ ?>
-				<tr>
-					<td class="text-center"> <?php echo $row['id'] ?> </td>
-					<td class="text-center"> <?php echo $row['objetivo'] ?> </td>
-					<td class="text-center"> <input type="checkbox" name="hola" value=SI> </td>
-					<td class="text-center"> <input type="checkbox" name="hola" value=SI> </td>
-					<td class="text-center"> <input type="checkbox" name="hola" value=SI> </td>
-				</tr>
-				<?php } ?>
+    <div class="container">
+        <h1><?php echo $numero; ?> : Control interno de bases de datos</h1>
+        <p><?php echo $descripcion; ?></p>
 
-			</tbody>
-		</table>
-	</div>
+
+
+		<h3 class="text-center"> Cuestionario de Objetivos de Control </h3>
+    <div class="table-responsive table/hover" id="TablaConsulta">
+        <table class="table">
+            <thead class="text-muted">
+                <th class="text-center">ID</th>
+                <th class="text-center">Objetivo</th>
+                <th class="text-center">Sí</th>
+                <th class="text-center">No</th>
+                <th class="text-center">N/A</th>
+            </thead>
+            <tbody>
+                <?php while($row = $save->fetch_assoc()){ ?>
+                <tr>
+                    <td class="text-center"> <?php echo $row['id'] ?> </td>
+                    <td class="text-center"> <?php echo $row['objetivo'] ?> </td>
+                    <td class="text-center"> <input type="checkbox" name="hola" value=SI> </td>
+                    <td class="text-center"> <input type="checkbox" name="hola" value=SI> </td>
+                    <td class="text-center"> <input type="checkbox" name="hola" value=SI> </td>
+                </tr>
+                <?php } ?>
+
+            </tbody>
+        </table>
+
+    </div>
+
+    </div>
 </body>
 </html>
--- proyecto_abd.sql
+++ proyecto_abd.sql
@@ -7,16 +7,14 @@
 -- Versión del servidor: 8.0.31
 -- Versión de PHP: 8.0.26
 
-
-create database proyecto_abd;
-
-use proyecto_abd;
-
+-- Drop and recreate the database
+DROP DATABASE IF EXISTS `proyecto_abd`;
+CREATE DATABASE `proyecto_abd`;
+USE `proyecto_abd`;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -32,6 +30,15 @@ SET time_zone = "+00:00";
 --
 -- Estructura de tabla para la tabla `contenido`
 --
+
+DROP TABLE IF EXISTS `Contenidos`;
+CREATE TABLE IF NOT EXISTS `Contenidos` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `numero` int NOT NULL,
+    `descripcion` VARCHAR(500),
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 DROP TABLE IF EXISTS `contenido`;
 CREATE TABLE IF NOT EXISTS `contenido` (
@@ -59,7 +66,22 @@ CREATE TABLE IF NOT EXISTS `cuestionario` (
 
 INSERT INTO `cuestionario` (`id`, `objetivo`) VALUES
 (1, 'AI1 Identificar Soluciones Automatizadas '),
-(4, 'AI2 Adquirir y Mantener Software Aplicativo');
+(2, 'AI2 Adquirir y Mantener Software Aplicativo');
+
+INSERT INTO `cuestionario` (`id`, `objetivo`) VALUES
+(3, 'AI3 ¿Define objetivos de control para asegurar la integridad y seguridad de las bases de datos? '),
+(4, 'AI4 ¿Sugiere prácticas y controles para garantizar la disponibilidad de la información en las bases de datos?');
+
+INSERT INTO `cuestionario` (`id`, `objetivo`) VALUES
+(5, 'AI5 ¿Se ha definido un marco de políticas y procedimientos para guiar la toma de decisiones y el cumplimiento de las regulaciones en TI?'),
+(6, 'AI6 ¿Se realizan revisiones periódicas de los niveles de servicio acordados y se toman medidas correctivas en caso de incumplimientos?');
+
+
+INSERT INTO `cuestionario` (`id`, `objetivo`) VALUES
+(7, ' ¿El manual de organización de la unidad administrativa está actualizado y correspondes con la estructura organizacional autorizada? '),
+(8, ' ¿Promueve la observancia del Código de Ética?');
+
+INSERT INTO `Contenidos` (`numero`, `descripcion`) VALUES (1,'El "Control Interno en los Sistemas Gestores de Bases de Datos" se refiere al conjunto de medidas y procedimientos diseñados para garantizar la seguridad, confiabilidad y eficiencia de los sistemas que administran y almacenan bases de datos. Estas medidas buscan asegurar que la información almacenada en la base de datos esté protegida contra accesos no autorizados, pérdida de datos y garantizar la precisión de los registros.');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
